290747c0f46523e616ad31e3b4d5fdef6e6d2ed9
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-09-24 17:08:18 +0300

    1.Peaple Counter App

--- COUNTER APP/index.js
+++ COUNTER APP/index.js
@@ -1,12 +1,19 @@
 
-
+let saveEl = document.getElementById('save-el');
 let countEl = document.getElementById('count-el');
 let count = 0;
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
 
+function save() {
+  let save = countEl.innerText;
+  let countStr = count +  '  -  ';
+  saveEl.innerText = saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
 
 
